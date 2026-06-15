f12b834804c1bd48ebb77258e8f4cfdb9d00c889
Zilfs <rheinlandcast@gmail.com>
2026-06-15 22:35:37 +0700

    feat (data): added mahasiswa data dummy

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+const mahasiswa = [
+  { nim: "001", nama: "Hirzi Ahnaf", jurusan: "Informatika" },
+  { nim: "002", nama: "Budi Santoso", jurusan: "Sistem Informasi" },
+  { nim: "003", nama: "Citra Dewi", jurusan: "Sains Data" },
+];
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
